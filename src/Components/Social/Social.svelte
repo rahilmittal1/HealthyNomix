3f30e6210f46a397aa8472246248a12d2edf02d2
Rahil Mittal <56689261+rahilmittal1@users.noreply.github.com>
2021-10-21 13:04:03 +0530

    Update Social.svelte

--- src/Components/Social/Social.svelte
+++ src/Components/Social/Social.svelte
@@ -11,7 +11,7 @@
     <div class="social-icons section-body">
       {#each IMAGES_LIST as list}
         <a
-          href="https://www.linkedin.com/in/nikhil-karkra-73a15319/"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank">
           <img src={list} alt="Social media {list}" />
         </a>
